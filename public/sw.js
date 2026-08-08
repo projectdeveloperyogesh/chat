fab9b7e0057756813cca8c8a2bb7a4755ef48db7
Yogesh <yogesh.hotwani@gmail.com>
2026-08-08 15:31:28 +0530

    Configure Android PWA, Capacitor Android config, and GitHub Actions Cloud APK Build Pipeline

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'yogesh-chat-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
